06ddfb3257735be4cb9f02bb478e24487671d465
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-09 13:47:05 -0600

    toy 2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      arr: ["Mike ", "Stuart ", "Joe ", "Spencer ", "Jayson ", "Nick"]
+    }
+  }
   render() {
+    let fam = this.state.arr.map((elm) => {
+      return <h2>{elm}</h2>
+    } )
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{fam}</h2>
       </div>
     );
   }
