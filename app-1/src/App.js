769b1d1a57bb4b5c45485a6390d89dbb607dee6a
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-09 13:37:08 -0600

    toy 1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      message: ""
+    }
+  }
+
+handleText(value){
+  this.setState({
+    message: value
+  })
+}
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" placeholder= "React Fun" onChange={event => this.handleText(event.target.value)}/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
