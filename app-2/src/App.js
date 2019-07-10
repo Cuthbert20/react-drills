a45718f9c787b4488301005528d252b68678acf4
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-10 13:38:46 -0600

    changing app-2 to map through image array

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,19 +6,24 @@ class App extends Component {
     super()
 
     this.state = {
-      arr: ["Mike ", "Stuart ", "Joe ", "Spencer ", "Jayson ", "Nick"]
+      arr: ["https://images.unsplash.com/photo-1562758624-44f3376ad1c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      "https://images.unsplash.com/photo-1500271910699-5c284ac9e2ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1325&q=80",
+      "https://images.unsplash.com/photo-1547286383-13a9dba6c67a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+      "https://images.unsplash.com/photo-1478034737238-29de3734b5b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+    ]
     }
   }
-  render() {
-    let fam = this.state.arr.map((elm) => {
-      return <h2>{elm}</h2>
-    } )
+  render(){
+    let arrDecon = this.state.arr.map(e => {
+      return <h2>{e}</h2>
+    })
     return (
       <div className="App">
-        <h2>{fam}</h2>
+        <h1>{arrDecon}</h1>
       </div>
     );
   }
 }
+  
 
 export default App;
